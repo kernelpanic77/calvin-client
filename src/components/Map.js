01403c4f207ae32f63006f625c7f78fb33a40bac
Pranav Reddy <spranavreddy25@gmail.com>
2021-03-27 01:44:08 +0530

    fml

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,30 +1,38 @@
-import React,{ useState } from 'react'
-import MapGL, {GeolocateControl } from 'react-map-gl'
-import 'mapbox-gl/dist/mapbox-gl.css'
+import React, { useState } from "react";
+import MapGL, { GeolocateControl } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-const TOKEN = process.env.REACT_APP_MAPBOX_TOKEN
+const TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
 const geolocateStyle = {
-  float: 'left',
-  margin: '50px',
-  padding: '10px'
+  float: "left",
+  margin: "50px",
+  padding: "10px",
 };
 
 const Map = () => {
-
-  const [viewport, setViewPort ] = useState({
+  const [viewport, setViewPort] = useState({
     width: "100%",
     height: 900,
     latitude: 0,
     longitude: 0,
-    zoom: 2
-  })
+    zoom: 2,
+  });
 
-  const _onViewportChange = viewport => setViewPort({...viewport, transitionDuration: 1000 })
-  
+  const _onViewportChange = (viewport) => {
+    setViewPort({ ...viewport, transitionDuration: 1000 });
+
+    // console.log(viewport);
+  };
+
   return (
-    <div style={{ margin: '0 auto'}}>
-      <h1 style={{textAlign: 'center', fontSize: '25px', fontWeight: 'bolder' }}>GeoLocator: Click To Find Your Location or click <a href="/search">here</a> to search for a location</h1>
+    <div style={{ margin: "0 auto" }}>
+      <h1
+        style={{ textAlign: "center", fontSize: "25px", fontWeight: "bolder" }}
+      >
+        GeoLocator: Click To Find Your Location or click{" "}
+        <a href="/search">here</a> to search for a location
+      </h1>
       <MapGL
         {...viewport}
         mapboxApiAccessToken={TOKEN}
@@ -33,12 +41,16 @@ const Map = () => {
       >
         <GeolocateControl
           style={geolocateStyle}
-          positionOptions={{enableHighAccuracy: true}}
+          positionOptions={{ enableHighAccuracy: true }}
           trackUserLocation={true}
+          onGeolocate={(viewport) => {
+            console.log(viewport);
+            return;
+          }}
         />
       </MapGL>
     </div>
-  )
-}
+  );
+};
 
-export default Map
+export default Map;
